2a3455ec749d243112d3d66eed6ec154d7d94487
Esteban Felipe <esteban.felipe@gmail.com>
2017-10-15 19:54:30 -0400

    Added generate file callback

--- server/api/generateFile/generateFile.controller.js
+++ server/api/generateFile/generateFile.controller.js
@@ -2,6 +2,7 @@
 
 import jsonpatch from 'fast-json-patch';
 import fs from 'fs';
+import request from 'request';
 
 var content = null;
 fs.readFile('server/api/generateFile/ISORequest.xml','utf8',function(err,data){
@@ -9,8 +10,28 @@ fs.readFile('server/api/generateFile/ISORequest.xml','utf8',function(err,data){
   content = data;
 });
 
+function callBack(caseId){
+	var body = content.replace(/00000/g,caseId);
+	request({
+		url:'http://bpms.everteam.us:8080/everteam/ode/processes/LaunchPointProcess_Processes_Core_ProcessISOCase_Process_ISO_Case_DCM',
+		headers:{
+			'Content-Type': 'text/xml; charset=utf-8'
+		},
+		method:'POST',
+		body:body	
+	},function(err,res,data){
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log(data);
+		}
+	});
+}
+
 export function create(req, res) {
 	res.status(200);
 	res.header("Content-Type","application/json");
 	res.send("OK");
+	setTimeout(callBack,3000,req.body.case_id)
 }
